fac198bc3cf8556505c3d378466a26f0d4998a05
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-14 18:25:34 +0200

    Delet file dist

--- src/server/dist/helpers.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emptyObject = void 0;
-const emptyObject = (obj) => Object.keys(obj).length === 0;
-exports.emptyObject = emptyObject;
--- src/server/dist/types/offerStatus.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.offerStatusEnum = void 0;
-exports.offerStatusEnum = { pending: "pending", active: "active", expired: "expired", deleted: "deleted" };
