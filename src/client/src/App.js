ad8996c894197d2e661268264293f098ecc7eb94
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-14 18:04:19 +0200

    Add Header with navBar &&  routes

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Offer from './pages/Offer.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import OfferCreator from './pages/OfferCreator.js';
 import Offers from './pages/Offers.js';
+import Header from './components/Header.js';
+import About from './pages/About.js';
+import Offer from './pages/Offer.js';
 
 function App() {
   return (
-    <div >
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/offer" element={<Offer />} />
-            <Route path="/offers" element={<Offers />} />
-          </Routes>
-        </Router>
-      </div>
+    <div className='app-main'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/create-offer" replace />} />
+          <Route path="/create-offer" element={<OfferCreator />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/offer/:offerId" element={<Offer />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
