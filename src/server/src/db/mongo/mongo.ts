7115571106ec41b7b1804024ce97cc03ad845aea
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-14 18:17:32 +0200

    Use process.env to get db infos

--- src/server/src/db/mongo/mongo.ts
+++ src/server/src/db/mongo/mongo.ts
@@ -5,8 +5,8 @@ import { mongoDbConfig } from './mongoDbConfig';
 export function create() {
 
   const getDbCtxt = async () => {
-    const dbName = "antiGaspiDb";
-    const cnxStr = 'mongodb+srv://AdminUser:UjD91xXOCYWtPVrj@cluster0.tadkbeh.mongodb.net/?retryWrites=true&w=majority'
+    const dbName = process.env.MONGO_DB_NAME as string;
+    const cnxStr = process.env.MONGO_DB_CONNECTION_STRING as string
     const client = new MongoClient(cnxStr, mongoDbConfig);
     const connectDb = await client.connect();
     return { /* client,  */db: connectDb.db(dbName) };
