7dc43e6993508f3ea2c1de6b018f1123c243e095
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-14 18:08:20 +0200

    Add offer contact page

--- src/client/src/pages/Offer.js
+++ src/client/src/pages/Offer.js
@@ -1,107 +1,83 @@
-import React from 'react';
-import { Formik, Form, ErrorMessage, Field } from 'formik';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Formik, Form/*, ErrorMessage, Field*/ } from 'formik';
 import * as Yup from 'yup';
-import dayjs from 'dayjs'
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-
+import FormikTextInput from '../components/FormikTextInput';
 
 function Offer() {
 
-    const initialValues = {
-        companyName: "", email: "", adress: "", description: "", availabilityDate: "", expirationDate: ""
+    const { offerId } = useParams();
+    // fetch offerId in data base if not existe return null 
+
+    let [isOfferExist, setIsOfferExist] = useState(false);
+
+    const fetchOffer = async (offerId) => {
+        const res = await fetch(`/offers/${offerId}`);
+        return await res.json() || {};
     };
+
+    useEffect(() => {
+        (async () => {
+            const offerInfo = await fetchOffer(offerId);
+            if (offerInfo.ok) {
+                setIsOfferExist(offerInfo.ok)
+            }
+        })();
+
+    }, [offerId]); // mais peut étre ça sera suprimée à la main en base ! danger
+
+    const initialValues = {
+        email: "", phone: "", message: ""
+    };
+
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
     const validationSchema = Yup.object().shape({
-        companyName: Yup.string().required("Le nom de la société est obligatoire"),
-        email: Yup.string().required("L'adresse e-mail est obligatoire ").email('Veuillez entrer une adresse e-mail valide'),
-        adress: Yup.string().required("L'adresse de la société est obligatoire"),
-        description: Yup.string().required("La déscription du matériel est obligatoire")
+        email: Yup.string().required("L'adresse e-mail est obligatoire").email('Veuillez entrer une adresse e-mail valide'),
+        phone: Yup.string().matches(phoneRegExp, "Veuillez entrer un numéro de téléphone valide").min(10, 'Le numéro de téléphone doit contenir 10 chiffres')
+            .max(10, 'Le numéro de téléphone doit contenir 10 chiffres').required("Le numéro de téléphone est obligatoire"),
+        message: Yup.string()
     });
 
-    const createOffer = async (values) => {
-        // Create offer
-        const addOfferRes = await fetch('/offers/add-offer', {
+    // Mise en contact 
+    const puttingInTouch = async (offerContactValues) => {
+        const addOfferRes = await fetch('/offers/contact', {
             method: 'POST',
-            body: JSON.stringify(values),
+            body: JSON.stringify(offerContactValues),
             headers: {
                 'Content-type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
             },
         });
-        const response = await addOfferRes.text()
-        console.log(' Add offer response=' + JSON.stringify(response));
-     
+        const response = await addOfferRes.json();
     };
 
-    const FormikTextInput = ({ fieldClassName, labelName, type, id, name, component, rows }) => {
-        return <div className='from-field-wrap'>
-            <div>
-                <label>
-                    <span className='form-label-field'> {labelName} </span>
-                </label>
-            </div>
-            <div>
-                <Field className={fieldClassName || 'tds-form-input'} type={type} id={id} name={name} rows={rows} component={component} />
-            </div>
-            <ErrorMessage name={name} className='form-label-field text-feedback' component="span" />
-        </div>
+    const onContactOfferFormSubmit = async (values) => {
+        const puttingInTouchRes = await puttingInTouch({ ...values, offerId });
     };
-    // "DD/MM/YYYY"  //  label={formikFieldLabel}              <ErrorMessage name={formikFieldName} className='form-label-field text-feedback' component="span" />
-    const FormikDateInput = ({ formik, formikFieldName, formikFieldLabel, dateFormat, spacing }) => {
-        return <div className='from-field-wrap'>
-            <div>
-                <label>
-                    <span className='form-label-field'> {formikFieldLabel} </span>
-                </label>
-            </div>
-            <div>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <Stack spacing={spacing}>
-                        <DesktopDatePicker
-                            name={formikFieldName}
-                            inputFormat={dateFormat}
-                            value={formik.values[formikFieldName]}
-                            onChange={(val) => formik.setFieldValue(`${formikFieldName}`,val)}
-                            renderInput={(params) => <TextField  {...params} />}
-                        />
-                    </Stack>
-                </LocalizationProvider>
-            </div>
-            {formik.errors[formikFieldName] ? <span className='form-label-field text-feedback'> </span> : null}
-        </div>
-    }
 
-    return (
-        <div>
-            <Formik initialValues={initialValues} onSubmit={createOffer} validationSchema={validationSchema} >
+    return <div>
+        {!isOfferExist ? null :
+            <Formik initialValues={initialValues} onSubmit={onContactOfferFormSubmit} validationSchema={validationSchema} >
                 {
                     (formik) => (
                         <Form className='form-offer'>
-                            <h1> Créer une annonce </h1>
+                            <h1> Contacter </h1>
                             <div className='align-horizontally '>
-                                <FormikTextInput fieldClassName='tds-form-input' labelName="Nom de la société*" type="text" id="companyName" name="companyName" component="input" />
                                 <FormikTextInput fieldClassName='tds-form-input' labelName="Email*" type="text" id="email" name="email" component="input" />
+                                <FormikTextInput fieldClassName='tds-form-input' labelName="Numéro de téléphone*" type="text" id="phone" name="phone" component="input" maxLength={10} />
+
                             </div>
-                            <FormikTextInput fieldClassName='tds-form-input' labelName="Adresse*" type="text" id="adress" name="adress" component="input" />
-                            <div className='align-horizontally '>
-                                <FormikTextInput fieldClassName="tds-form-textarea-input" labelName="Matériel/description*" type="text" id="description" name="description" component="textarea" rows={10} />
-                                <div className='align-vertically'>
-                                    <FormikDateInput formik={formik} formikFieldName="availabilityDate" formikFieldLabel="Dispo à partir de*" dateFormat="DD/MM/YYYY" spacing={3} />
-                                    <FormikDateInput formik={formik} formikFieldName="expirationDate" formikFieldLabel="Expriration le*" dateFormat="DD/MM/YYYY" spacing={3} />
-                                </div>
-                            </div>
+                            <FormikTextInput fieldClassName="tds-form-textarea-input" labelName="Message" type="text" id="message" name="message" component="textarea" rows={10} />
                             <div className='from-field-wrap'>
-                                <button disabled={!formik.isValid} className='create-btn' >Créer l'annonce</button>
+                                <button disabled={!formik.isValid} className='btn' >Mise en contact</button>
                             </div>
                         </Form>
                     )
                 }
             </Formik>
-        </div>
-    );
+        }
+    </div>;
 }
 
 export default Offer;
