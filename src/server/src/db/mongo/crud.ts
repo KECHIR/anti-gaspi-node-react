35e064f82fe08b73befc530a8f4835e943de9473
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-14 18:16:14 +0200

    Add crud functions

--- src/server/src/db/mongo/crud.ts
+++ src/server/src/db/mongo/crud.ts
@@ -1,12 +1,31 @@
 export function create(dbCtxt: any) {
   // colName : collection name 
   function col(colName: string) {
-    return dbCtxt?.db?.collection(colName)
+    return dbCtxt?.db?.collection(colName);
   }
 
-  function insert(colName: string, document: object) {
-    return col(colName).insert(document)
+  async function insert(colName: string, document: object) {
+    return col(colName).insert(document);
   }
 
-  return { insert };
+  async function insertOne(colName: string, document: object) {
+    return col(colName).insertOne(document);
+  }
+
+  async function find(colName: string, query: object) {
+    const data = await col(colName).find(query);
+    return await data.toArray();
+  }
+
+  async function findOne(colName: string, query: object) {
+    const data = await col(colName).findOne(query) || {};
+    return data;
+  }
+
+  async function updateOne(colName: string, findQuery: object, updateQuery: object) {
+    const data = await col(colName).updateOne(findQuery, updateQuery) || {};
+    return data;
+  }
+
+  return { insert, find, findOne, insertOne, updateOne };
 }
