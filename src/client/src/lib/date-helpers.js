e323ddb50aa43dfb5c86488d9f9891317776889b
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-17 10:19:47 +0200

    Add UTCToday function

--- src/client/src/lib/date-helpers.js
+++ src/client/src/lib/date-helpers.js
@@ -1,1 +1,18 @@
-export const toDate = (d) => new Date(d);
+
+export function UTCDate(y, mth, d, h, m, s, ms) {
+    var utcms = Date.UTC(y, mth, d, h || 0, m || 0, s || 0, ms || 0);
+    return new Date(utcms);
+}
+
+export function onlyDate(d) {
+    if (!d) {
+        return d;
+    }
+    return UTCDate(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+}
+
+export function UTCToday() { //quelle sémantique?! actuellement correpond au today local exprimé en utc => s 'il est le 2/2/2013 00:30 à paris. ça retournera 2/2/2013 en utc. alors qu'il n'est encore que 1/2/2013 11:30 en UTC!
+    return onlyDate(new Date());
+}
+
+export const dateFormat = "DD/MM/YYYY";
