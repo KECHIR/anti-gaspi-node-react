0c1cd082e702b116fbb01c8bb9069cf35b083153
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-17 10:18:53 +0200

    Show expiration date

--- src/client/src/components/OfferItem.js
+++ src/client/src/components/OfferItem.js
@@ -1,18 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import { showDate } from '../lib/moment.js';
 
 export default function OfferItem({ offerItemData }) {
     const { _id: offerId, data } = offerItemData || {};
 
     const navigate = useNavigate();
     const navigateToContactOffer = () => navigate(`/offer/${offerId}`)
-    
-    const { adress, description } = data || {};
+
+    const { adress, description, expirationDate } = data || {};
     return <div className="offer-item">
         <div className="offer-item-padding">{adress}</div>
         <div className="offer-item-padding">{description}</div>
         <div className="offer-item-bottom offer-item-padding">
             <button className='btn' onClick={navigateToContactOffer} >Contacter</button>
-            <div> Expire le 30/10/2022 </div>
+            <div> Expire le {`${showDate(expirationDate)}`} </div>
         </div>
     </div>
 }
