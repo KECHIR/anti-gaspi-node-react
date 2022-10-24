0a4ec8773daab3320a906ba3bec65a3e6b96ca22
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-24 10:47:48 +0200

    Change idx to _id to optimize real dom vs virtual dom

--- src/client/src/components/OfferList.js
+++ src/client/src/components/OfferList.js
@@ -3,6 +3,6 @@ import OfferItem from "./OfferItem"
 export default function OffersList({ offersListData }) {
     return <div className="offers-list">
         <h1> Annonces </h1>
-        {offersListData.map((offerItemData,idx) => <OfferItem key={idx} offerItemData={offerItemData} />)}
+        {offersListData.map((offerItemData) => <OfferItem key={offerItemData._id} offerItemData={offerItemData} />)}
     </div>
 }
