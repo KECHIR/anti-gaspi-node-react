b28c1900d0e40323b35fe4fa69bfb6303597726d
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-24 10:46:35 +0200

    Refactor code import validaTionSchema and initialValues from models

--- src/client/src/pages/Offer.js
+++ src/client/src/pages/Offer.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Formik, Form/*, ErrorMessage, Field*/ } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form } from 'formik';
 import FormikTextInput from '../components/FormikTextInput';
 import AlertNotification from '../components/AlertNotification.js';
 import { create as createOfferService } from '../services/offerService.js';
+import { validationSchema, initialValues } from '../models/offerContactModel.js';
 
 function Offer() {
 
     const { offerId } = useParams();
+    const offerService = createOfferService();
     // fetch offerId in data base if not existe return null 
-
     let [isOfferExist, setIsOfferExist] = useState(false);
 
     const initAlertNotificationInfos = { ok: false };
     const [alertNotificationInfos, setAlertNotificationInfos] = useState(initAlertNotificationInfos);
-    const offerService = createOfferService();
 
     const handleCloseAlerNotification = (event, reason) => {
         if (reason === 'clickaway') {
@@ -33,19 +32,6 @@ function Offer() {
         })();
 
     }, [offerId, offerService]); // mais peut étre ça sera suprimée à la main en base ! danger
-
-    const initialValues = {
-        email: "", phone: "", message: ""
-    };
-
-    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
-
-    const validationSchema = Yup.object().shape({
-        email: Yup.string().required("L'adresse e-mail est obligatoire").email('Veuillez entrer une adresse e-mail valide'),
-        phone: Yup.string().matches(phoneRegExp, "Veuillez entrer un numéro de téléphone valide").min(10, 'Le numéro de téléphone doit contenir 10 chiffres')
-            .max(10, 'Le numéro de téléphone doit contenir 10 chiffres').required("Le numéro de téléphone est obligatoire"),
-        message: Yup.string()
-    });
 
     const onContactOfferFormSubmit = async (values) => {
         const puttingInTouchRes = await offerService.puttingInTouch({ ...values, offerId });
