613a4cfb0bbd56714c12bde1a20392307bffa0ae
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-24 10:48:38 +0200

    Change contact button to Link

--- src/client/src/components/OfferItem.js
+++ src/client/src/components/OfferItem.js
@@ -1,18 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { showDate } from '../lib/moment.js';
 
 export default function OfferItem({ offerItemData }) {
     const { _id: offerId, data } = offerItemData || {};
-
-    const navigate = useNavigate();
-    const navigateToContactOffer = () => navigate(`/offer/${offerId}`)
-
     const { adress, description, expirationDate } = data || {};
     return <div className="offer-item">
         <div className="offer-item-padding">{adress}</div>
         <div className="offer-item-padding">{description}</div>
         <div className="offer-item-bottom offer-item-padding">
-            <button className='btn' onClick={navigateToContactOffer} >Contacter</button>
+            <Link to={`/offer/${offerId}`} >
+                <button className='btn'  >
+                    Contacter
+                </button>
+            </Link>
             <div> Expire le {`${showDate(expirationDate)}`} </div>
         </div>
     </div>
