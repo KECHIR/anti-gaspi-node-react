076fcf23f18778231c20e5b3b68d28405736f711
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-24 10:44:58 +0200

    Refector code import FormikDateInput from components

--- src/client/src/pages/OfferCreator.js
+++ src/client/src/pages/OfferCreator.js
@@ -1,50 +1,16 @@
 import React, { useState } from 'react';
-import { Formik, Form/*, ErrorMessage, Field*/ } from 'formik';
-import * as Yup from 'yup';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { isValidDate, addDays } from '../lib/moment.js';
-import { UTCToday, dateFormat } from '../lib/date-helpers';
-import "moment/locale/fr"
+import { Formik, Form } from 'formik';
+import { dateFormat } from '../lib/date-helpers';
 import FormikTextInput from '../components/FormikTextInput.js';
 import AlertNotification from '../components/AlertNotification.js';
+import FormikDateInput from '../components/FormikDateInput.js';
 import { create as createOfferService } from '../services/offerService.js';
+import { validationSchema, initialValues } from '../models/offerModel.js';
 
 function OfferCreator() {
 
-    const dateNow = UTCToday();
-
     const offerService = createOfferService();
-
-    const invalidDateMsg = "La date n'est pas valide";
-
-    const initialValues = {
-        companyName: "", email: "", adress: "", description: "", availabilityDate: dateNow, expirationDate: dateNow
-    };
-
-    const validationSchema = Yup.object().shape({
-        companyName: Yup.string().required("Le nom de la société est obligatoire"),
-        email: Yup.string().required("L'adresse e-mail est obligatoire ").email('Veuillez entrer une adresse e-mail valide'),
-        adress: Yup.string().required("L'adresse de la société est obligatoire"),
-        description: Yup.string().required("La déscription du matériel est obligatoire"),
-        availabilityDate: Yup.date().min(addDays(dateNow, -1), 'Veuillez choisir une date future').typeError(invalidDateMsg),
-        expirationDate: Yup
-            .date().required('La date expiration est obligatoire')
-            .when(
-                "availabilityDate",
-                (availabilityDate, schema) => {
-                    if (availabilityDate && isValidDate(availabilityDate)) {
-                        return schema.min(availabilityDate, "La date d’expiration doit être supérieure à la date de disponibilité")
-                    }
-                }
-            ).typeError(invalidDateMsg)
-    });
-
-    validationSchema.cast({ availabilityDate: dateNow, expirationDate: dateNow });
-
+    //########## Handle toast
     const initAlertNotificationInfos = { ok: false };
     const [alertNotificationInfos, setAlertNotificationInfos] = useState(initAlertNotificationInfos);
 
@@ -65,35 +31,8 @@ function OfferCreator() {
         }
     };
 
-    // "DD/MM/YYYY"  //  label={formikFieldLabel}              <ErrorMessage name={formikFieldName} className='form-label-field text-feedback' component="span" />
-    const FormikDateInput = ({ formik, formikFieldName, formikFieldLabel, dateFormat, spacing }) => {
-        return <div className='from-field-wrap'>
-            <div>
-                <label>
-                    <span className='form-label-field'> {formikFieldLabel} </span>
-                </label>
-            </div>
-            <div>
-                <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="fr"  >
-                    <Stack spacing={spacing}>
-                        <DesktopDatePicker
-                            disablePast={true}
-                            name={formikFieldName}
-                            inputFormat={dateFormat}
-                            value={formik.values[formikFieldName]}
-                            onChange={(val) => formik.setFieldValue(`${formikFieldName}`, val)}
-                            renderInput={(params) => <TextField  {...params} />}
-                        />
-                    </Stack>
-                </LocalizationProvider>
-            </div>
-            {formik.errors[formikFieldName] ? <span className='form-label-field text-feedback'> {formik.errors[formikFieldName]} </span> : null}
-        </div>
-    }
-
     return (
         <div>
-
             {alertNotificationInfos.ok ? <AlertNotification severity={alertNotificationInfos.severity} open={true} message={alertNotificationInfos.message} handleClose={handleCloseAlerNotification} /> : null}
             <Formik initialValues={initialValues} onSubmit={createOffer} validationSchema={validationSchema} >
                 {
